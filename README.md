24cdc8185e037a21159d4f3c48443bc703a58856
Mikhail Kozkin <Mikhail.Kozkin@yandex.kz>
2025-03-22 01:47:04 -0700

    check 5 sprint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/Misha-kozkin/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function cardCreation(elements) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const placesList = document.querySelector('.places__list');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__title').textContent = elements.name;
+  cardElement.querySelector('.card__image').src = elements.link;
+
+  deleteButton.addEventListener('click', deleteCard);
+
+  placesList.append(cardElement);
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  const card = evt.target.closest('.card');
+  if (card) {
+    card.remove();
+  };
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  cardCreation(item);
+});
